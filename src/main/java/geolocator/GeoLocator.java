482eff074e0228c8273590cddaeb51ff146086eb
krook1024 <krook1024@gmail.com>
2020-03-27 21:05:07 +0100

    Implemented looging in GeoLocator class

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -12,7 +12,12 @@ import org.apache.commons.io.IOUtils;
 
 import org.apache.commons.io.IOUtils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GeoLocator {
+
+    private static Logger logger = LoggerFactory.getLogger(GeoLocator.class);
 
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
@@ -29,11 +34,16 @@ public class GeoLocator {
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
             url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            logger.debug("Got IP address {}", ipAddrOrHost);
         } else {
             url = new URL(GEOLOCATOR_SERVICE_URI);
+            logger.debug("Got no IP address or host, using the address of the host");
         }
+        logger.info("Downloading JSON information from {}", url);
         String s = IOUtils.toString(url, "UTF-8");
-        return GSON.fromJson(s, GeoLocation.class);
+        GeoLocation response = GSON.fromJson(s, GeoLocation.class);
+        logger.debug("Got JSON response: {}", response);
+        return response;
     }
 
     public static void main(String[] args) throws IOException {
@@ -41,7 +51,7 @@ public class GeoLocator {
             String arg = args.length > 0 ? args[0] : null;
             System.out.println(new GeoLocator().getGeoLocation(arg));
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            logger.error("Something is wrong", e);
         }
     }
 
